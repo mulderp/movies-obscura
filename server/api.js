a749ca705417123b54f57849e09381dbb5398fa1
Patrick Mulder <mulder.patrick@gmail.com>
2013-10-15 21:20:39 +0200

    use redis url

--- server/api.js
+++ server/api.js
@@ -1,8 +1,22 @@
 var restify = require('restify');
 var _ = require('underscore');
 var fs = require('fs');
-var redis = require('redis'),
-    client = redis.createClient();
+var url = require('url');
+var querystring = require('querystring');
+
+// redis
+  var password, database;
+  var parsed_url  = url.parse(process.env.REDISTOGO_URL || 'redis://localhost:6379');
+  var parsed_auth = (parsed_url.auth || '').split(':');
+  var options = querystring.parse(parsed_url.query);
+
+  var client = require('redis').createClient(parsed_url.port, parsed_url.hostname, options);
+
+  if (password = parsed_auth[1]) {
+    client.auth(password, function(err) {
+      if (err) throw err;
+    });
+  }
 
 var movies = require('./movies.js');
 
